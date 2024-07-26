1985e05db2f91c0978f27ab43b22efdbae1270a7
takeishi-g <89860661+takeishi-g@users.noreply.github.com>
2024-07-26 22:48:43 +0900

    a

--- app/_components/Menu/index.tsx
+++ app/_components/Menu/index.tsx
@@ -19,7 +19,7 @@ function Menu() {
             <Link href="/news">ニュース</Link>
           </li>
           <li>
-            <Link href="members">メンバー</Link>
+            <Link href="/members">メンバー</Link>
           </li>
           <li>
             <Link href="/contact">お問い合わせ</Link>
