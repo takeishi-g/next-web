4b16793b6a090d79a79dc6075fca9f255d2111e8
takeishi-g <89860661+takeishi-g@users.noreply.github.com>
2024-07-27 11:08:55 +0900

    a

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,6 +1,7 @@
 import NewsList from "../_components/NewsList";
 import { getNewsList } from "../_libs/microcms";
 
+export const revalidate = 60
 
 export default async function Page() {
   const { contents: news } = await getNewsList()
